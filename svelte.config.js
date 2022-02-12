3ce842af5b90366fe1224412b4a22dc70f59dfb7
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-02-12 10:48:14 -0600

    refactor for netlify deploys

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-netlify';
+// const mode = process.env.NODE_ENV;
+// const dev = mode === 'development';
 
-/** @type {import('@sveltejs/kit').Config} */
+// process.env.TAILWIND_MODE = dev ? 'watch' : 'build';
+
 const config = {
-	kit: {
-		adapter: adapter(),
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
-	}
+  kit: {
+    adapter: adapter({
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app
+			split: false
+		})
+  }
 };
 
-export default config;
+export default config; // Workaround until SvelteKit uses Vite 2.3.8 (and it's confirmed to fix the Tailwind JIT problem)
