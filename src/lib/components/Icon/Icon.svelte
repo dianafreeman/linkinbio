1e41f3e6acaeebe3537000ac405bdcae51d70dc8
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2022-02-26 11:37:52 -0600

    consolidate icons into one folder

---
+++ src/lib/components/Icon/Icon.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let type, color, glow, wrapperClass;
+
+  const ICON_TYPE_MAP = {
+    youtube: () => import('$lib/components/Icon/icons/youtube.svelte'),
+    bookmark: () => import('$lib/components/Icon/icons/bookmark.svelte'),
+    handshake: () => import('$lib/components/Icon/icons/handshake.svelte'),
+    github: () => import('svelte-icons/fa/FaGithub.svelte'),
+    linkedin: () => import('svelte-icons/fa/FaLinkedinIn.svelte'),
+    checkmark: () => import('$lib/components/Icon/icons/checkmark.svelte'),
+    newwindow: () => import('$lib/components/Icon/icons/newwindow.svelte'),
+    tiktok: () => import('$lib/components/Icon/icons/tiktok.svelte'),
+  };
+</script>
+
+{#await ICON_TYPE_MAP[type]() then module}
+  <div class={wrapperClass || 'w-20 h-10 m-auto '}>
+    <svelte:component this={module.default} {...$$props} {type} {color} {glow} />
+  </div>
+{/await}
